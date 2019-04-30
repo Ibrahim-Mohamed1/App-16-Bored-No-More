9086701ff839d4f327513441118db03319bfc0f9
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-04-30 16:25:38 -0600

    bored no more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css"
+import { withData } from './DataProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    console.log(this.props.cat)
+    const styles={
+      cat:{
+        width: "90%",
+        display:"block",
+        margin:"auto"
+      },
+      button:{
+        display: "block",
+        margin: "auto",
+        padding: 10,
+        backgroundColor: '#ffffff00',
+        borderRadius: 5,
+        fontSize: 20,
+        color: "black",
+        border: 'solid black 2px'
+      }
+    }
+    return (
+      <div>
+        <h1 style={{color: "black", textAlign:'center', fontSize: "3em", fontFamily: 'cursive', textDecoration: "underline", textDecorationColor: "gold"}}>Bored No More!</h1>
+        <h1 style={{color: "black", textAlign:'center', fontFamily: 'cursive'}}>Find something to do!</h1>
+        <button style={styles.button} onClick={() => this.props.getCat()}>Something else</button>
+        <div style={{backgroundImage:"url()"}}>
+          <h1 style={{color: 'gold', textAlign:'center', backgroundColor: '#0000005e'}}>{this.props.cat}.</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withData(App);
